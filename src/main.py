4786aa4620d0085eff2b1aa248d4a74dc82470f5
SJC <mr3coi@gmail.com>
2018-07-13 17:57:10 +0900

    Implemented gaussian noise/sine noises; produced outputs

--- src/main.py
+++ src/main.py
@@ -31,6 +31,8 @@ def get_parser():
                         help="Create a white blank canvas, instead of using an image")
     parser.add_argument('-g', "--gamma", type=float, default=1,
                         help="Do gamma correction on the given input (default: 1 => no correction)")
+    parser.add_argument('-t', "--type", type=str, default='fixed',
+                        help="Type of pattern to generate.")
 
     return parser
 
@@ -49,10 +51,10 @@ def main():
     H, W, _ = canvas.shape
 
     # ================================== Add operations here =====================================
-    canvas = linear_wave(canvas,contrast=10,skew=80)
-    canvas = linear_wave(canvas,contrast=10,skew=20)
-    canvas = linear_wave(canvas,contrast=10,skew=20,rowwise=False)
-    canvas = linear_wave(canvas,contrast=10,skew=80,rowwise=False)
+    canvas = linear_wave(canvas,skew=80, pattern=args.type,contrast=10,dev=10//3)
+    canvas = linear_wave(canvas,skew=20, pattern=args.type,contrast=10,dev=10//3)
+    canvas = linear_wave(canvas,skew=20,rowwise=False, pattern=args.type,contrast=10,dev=10//3)
+    canvas = linear_wave(canvas,skew=80,rowwise=False, pattern=args.type,contrast=10,dev=10//3)
 
     '''
     dither(canvas,gap=10, skew=50)
@@ -85,7 +87,19 @@ def main():
         save_name = args.save + '_{:.4f}.{}'.format(psnr_val, args.save_format)
         cv2.imwrite(os.path.join(args.savepath, save_name), canvas)
 
-def linear_wave(canvas, color=None, contrast=1, gap=5, skew=0, thick=2, rowwise=True):
+def linear_wave(canvas, gap=5, skew=0, thick=2, rowwise=True,
+                pattern='fixed', contrast=1, color=None, dev=1, seed=0):
+    '''
+    :param pattern: Pattern for the noise to add to each line \
+                    ('fixed', 'gaussian', 'sine')
+    :type pattern: str
+    :param contrast: (mean) value to add to the given image before normalization \
+                        (mean for 'gaussian', middle value for 'sine', value for 'fixed'). \
+                     The same value is set for all channels; use 'color' to provide different values.
+    :type contrast: int
+    :param dev: deviation (stddev for 'gaussian', amplitude for 'sine')
+    :type dev: float
+    '''
     mask_shape = list(canvas.shape)
     if rowwise:
         mask_shape[0] += 2 * np.abs(skew)
@@ -95,14 +109,34 @@ def linear_wave(canvas, color=None, contrast=1, gap=5, skew=0, thick=2, rowwise=
     H, W, _ = mask_shape
     if color is None:
         color = (contrast,) * 3
+
+    # Generate color map
+    num_lines = len(list(range(0,H,gap))) if rowwise else len(list(range(0,W,gap)))
+    if pattern=='gaussian':
+        np.random.seed(seed)
+        color_map = [[int(round(np.clip(np.random.randn() * dev + mean, \
+                        max(mean-2*dev,0), min(mean+2*dev,255)))) \
+                            for mean in color]
+                            for _ in range(num_lines)]
+    elif pattern == 'sine':
+        color_map = [[int(round(np.clip(np.sin(line_num) * dev + mean, 0, 255))) \
+                                for mean in color] \
+                            for line_num in range(num_lines)]
+
+    elif pattern == 'fixed':
+        color_map = [color,] * num_lines
+    else:
+        raise NotImplementedError('Please choose a valid pattern type.')
+
+    # Add noise
     if rowwise:
-        for row in range(0,H,gap):
+        for row, color in zip(range(0,H,gap), color_map):
             if skew <= 0:
                 cv2.line(mask,(0,row),(W,row+skew),color,thickness=thick)
             else:
                 cv2.line(mask,(0,row-skew),(W,row),color,thickness=thick)
     else:
-        for col in range(0,W,gap):
+        for col, color in zip(range(0,W,gap), color_map):
             if skew <= 0:
                 cv2.line(mask,(col,0),(col+skew,H),color,thickness=thick)
             else:
@@ -111,6 +145,25 @@ def linear_wave(canvas, color=None, contrast=1, gap=5, skew=0, thick=2, rowwise=
         mask = mask[np.abs(skew):-np.abs(skew)]
     else:
         mask = mask[:,np.abs(skew):-np.abs(skew)]
+    '''
+    else:
+        if rowwise:
+            for row in range(0,H,gap):
+                if skew <= 0:
+                    cv2.line(mask,(0,row),(W,row+skew),color,thickness=thick)
+                else:
+                    cv2.line(mask,(0,row-skew),(W,row),color,thickness=thick)
+        else:
+            for col in range(0,W,gap):
+                if skew <= 0:
+                    cv2.line(mask,(col,0),(col+skew,H),color,thickness=thick)
+                else:
+                    cv2.line(mask,(col-skew,0),(col,H),color,thickness=thick)
+        if rowwise:
+            mask = mask[np.abs(skew):-np.abs(skew)]
+        else:
+            mask = mask[:,np.abs(skew):-np.abs(skew)]
+    '''
 
     out = cv2.normalize(canvas+mask, dst=None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
     return np.uint8(out * 255)
